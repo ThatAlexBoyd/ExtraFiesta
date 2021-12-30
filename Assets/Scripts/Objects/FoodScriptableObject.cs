57bd9ab3f03b8c18832692e0dbe16aab28d41574
Alex Boyd <alexjboyd@gmail.com>
2021-12-30 14:28:19 -0500

    SO for foods and ingredients

---
+++ Assets/Scripts/Objects/FoodScriptableObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Food", menuName = "Food")]
+public class FoodScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    private int id;
+
+    [SerializeField]
+    private string displayName;
+
+    [SerializeField]
+    [TextArea(1, 5)]
+    private string description;
+
+    [SerializeField]
+    private int price; //sold price
+
+    [SerializeField]
+    private List<RecipeItems> recipe = new List<RecipeItems>();
+}
+
+[System.Serializable]
+public class RecipeItems
+{
+    [SerializeField]
+    private IngredientScriptableObject ingredient;
+
+    [SerializeField]
+    private int amount;
+}
---
+++ Assets/Scripts/Objects/IngredientScriptableObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient", menuName = "Ingredient")]
+public class IngredientScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    private int id;
+
+    [SerializeField]
+    private string displayName;
+
+    [SerializeField]
+    [TextArea(1, 5)]
+    private string description;
+
+    [SerializeField]
+    private int cost;
+
+    [SerializeField]
+    private int quantity;
+}
